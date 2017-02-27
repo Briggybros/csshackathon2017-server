095711749acdeaff669f15ec389a1fe00f4b3498
Ben Norris <bn15932@my.bristol.ac.uk>
2017-02-27 13:02:49 +0000

    added AuthModel and index

---
+++ database/index.js
@@ -0,0 +1,54 @@
+const Sequelize = require('sequelize');
+const {AuthModel} = require('./models/AuthModel.js');
+
+/** A class representing the database */
+export class Database {
+
+
+  /**
+  * @constructor
+  * @param {String} env The current environment.
+  */
+  constructor(env) {
+    this.createSequelize(env)
+      .then(() => {
+        return this.createModels();
+      })
+      .then(() => {
+        this.setupRelations().catch(console.log);
+      });
+  }
+
+  /**
+  * @param {String} env The current environment.
+  * @return {Promise} A promise of a created database.
+  */
+  createSequelize(env) {
+    /* Use promise to return once database querying is complete. */
+
+      //
+      // // /* Make all users have people they're following */
+      // // this.followersModel.Follower.belongsTo(this.userModel.User, {
+      // //   foreignKey: {
+      // //     field: 'userId',
+      // //     allowNull: false,
+      // //   },
+      // //   onDelete: 'CASCADE',
+      // // });
+      // /* Make all followers be following a user  */
+      // this.userModel.User.hasMany(this.this.userModel.User
+      //   // foreignKey: {
+      //   //   field: 'followingId',
+      //   //   allowNull: false,
+      //   // },
+      //   // onDelete: 'CASCADE',
+      // );
+      resolve();
+    });
+  }
+
+  /** Cleanly close the database */
+  close() {
+    this.sequelize.close();
+  }
+}
---
+++ database/models/AuthModel.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize');
+
+/* eslint new-cap: ["error", {"capIsNew": false}] */
+
+/** Class representing a Pushendpoint model for sequelize. */
+export class AuthModel {
+
+    /**
+    * @constructor
+    * @param {Object} sequelize The Sequelize object.
+    */
+  constructor(sequelize) {
+    this.Auth = sequelize.define('auth', {
+      id: {
+        primaryKey: true,
+        type: Sequelize.STRING,
+        field: 'id',
+        allowNull: false,
+        unique: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        field: 'name',
+      },
+    }, {
+      freezeTableName: true, //Model tableName will be same as model name
+    });
+  }
+}
