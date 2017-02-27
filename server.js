17a9c1a47e2138cd372972b808ab548082dbbb15
Briggybros <briggybros.2@googlemail.com>
2017-02-27 13:20:43 +0000

    added get county from position

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const request = require('request');
+
 const app = express();
 
 const port = 8081 || process.env.PORT;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/getauthority', (req, res) => {
+  request(`http://maps.googleapis.com/maps/api/geocode/json?latlng=${req.query.latitude},${req.query.longitude}&sensor=false`, (error, response, body) => {
+    if(!error && response.statusCode == 200) {
+      let result = JSON.parse(body).results[0].address_components;
+      let response = '';
+      for (let i = 0; i < result.length; i++) {
+        for (let j = 0; j < result[i].types.length; j++) {
+          if (result[i].types[j] === 'administrative_area_level_2') {
+            response = result[i].long_name;
+          }
+        }
+      }
+      res.send(response);
+    }
+  });
 });
 
 app.listen(port, () => {
