7cea1a72bcec9475c4e7237700fd7a4e69f6e01e
Ben Norris <bn15932@my.bristol.ac.uk>
2017-02-27 14:36:43 +0000

    a great big bunch of models

--- database/index.js
+++ database/index.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize');
 const {AuthModel} = require('./models/AuthModel.js');
 const {MaterialsModel} = require('./models/MaterialsModel.js');
 const {ItemModel} = require('./models/ItemModel.js');
+const {InstructionModel} = require('./models/InstructionModel.js');
 
 /** A class representing the database */
 export class Database {
@@ -22,11 +23,90 @@ export class Database {
   }
 
   /**
-  * @param {String} env The current environment.
-  * @return {Promise} A promise of a created database.
+* @param {String} env The current environment.
+* @return {Promise} A promise of a created database.
+*/
+createSequelize(env) {
+  /* Use promise to return once database querying is complete. */
+  return new Promise((resolve, reject) => {
+    if (env === 'dev') {
+      this.sequelize = new Sequelize('recyclapple', 'recyclapple', 'password', {
+        dialect: 'sqlite',
+        storage: './database.sqlite',
+      });
+    } else {
+      this.sequelize = new Sequelize('recyclapple', 'recyclapple', 'password', {
+        host: 'localhost',
+        dialect: 'mysql',
+        logging: false,
+
+        pool: {
+          max: 5,
+          min: 0,
+          idle: 10000,
+        },
+      });
+    }
+    resolve();
+  });
+}
+
+/**
+  * @return {Promise} A promise of a database with tables.
   */
-  createSequelize(env) {
+  createModels() {
     /* Use promise to return once database querying is complete. */
+    return new Promise((resolve, reject) => {
+      this.authModel = new AuthModel(this.sequelize);
+      this.itemModel = new ItemModel(this.sequelize);
+      this.instructionModel = new InstructionModel(this.sequelize);
+      this.materialsModel = new MaterialsModel(this.sequelize);
+      resolve();
+    });
+  }
+
+  /**
+  * @return {Promise} A promise of a database with foreignKey relationships.
+  */
+  setupRelations() {
+    /* Use promise to return once database querying is complete. */
+    return new Promise((resolve, reject) => {
+      /* Make all events belong to some user. */
+      this.instructionModel.Instruction.belongsTo(this.authModel.Auth, {
+        foreignKey: {
+          field: 'authId',
+          allowNull: false,
+        },
+        onDelete: 'CASCADE',
+      });
+      this.instructionModel.PushEndpoint.belongsTo(this.userModel.User, {
+         foreignKey: {
+           field: 'userId',
+           allowNull: false,
+         },
+         onDelete: 'CASCADE',
+       });
+
+
+      this.userModel.User.belongsToMany(this.userModel.User, {
+        as: 'Follower',
+        through: {
+          model: this.followerModel.Follower,
+          unique: false,
+        },
+        foreignKey: 'follower',
+        constraints: false,
+      });
+
+      this.userModel.User.belongsToMany(this.userModel.User, {
+        as: 'Following',
+        through: {
+          model: this.followerModel.Follower,
+          unique: false,
+        },
+        foreignKey: 'following',
+        constraints: false,
+      });
 
       //
       // // /* Make all users have people they're following */
---
+++ database/models/InstructionModel.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize');
+
+/* eslint new-cap: ["error", {"capIsNew": false}] */
+
+/** Class representing a Pushendpoint model for sequelize. */
+export class InstructionModel {
+
+    /**
+    * @constructor
+    * @param {Object} sequelize The Sequelize object.
+    */
+  constructor(sequelize) {
+    this.Instruction = sequelize.define('instruction', {
+      id: {
+        primaryKey: true,
+        type: Sequelize.STRING,
+        field: 'id',
+        allowNull: false,
+        unique: true,
+      },
+      instruction: {
+        type: Sequelize.STRING,
+        field: 'instruction',
+      },
+    }, {
+      freezeTableName: true, //Model tableName will be same as model name
+    });
+  }
+}
